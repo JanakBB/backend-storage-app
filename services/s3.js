e6d11b6a7320b1457137e44f9d5f14c1d5e9be77
Janak Bahadur Bohara <janakcustomx@gmail.com>
2025-11-25 19:54:23 +0545

    update 20

--- services/s3.js
+++ services/s3.js
@@ -9,7 +9,7 @@ import {
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3Client = new S3Client({
-  region: "ap-south-1",
+  region: process.env.AWS_REGION || "us-east-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
