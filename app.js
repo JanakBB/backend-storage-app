36585cf54d2280384e21997f506a2cd217fbb2d4
Janak Bahadur Bohara <janakcustomx@gmail.com>
2025-11-25 20:19:02 +0545

    update app.js cors 1

--- app.js
+++ app.js
@@ -88,7 +88,12 @@ async function initializeApp() {
         },
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-        allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+        allowedHeaders: [
+          "Content-Type",
+          "Authorization",
+          "X-Requested-With",
+          "dirname", // ← ADD THIS LINE
+        ],
       })
     );
 
