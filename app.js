7c0b4005cc0c521a596d6fd2d6f2a9abb79ac390
Janak Bahadur Bohara <janakcustomx@gmail.com>
2025-12-07 16:49:39 +0545

    update our1

--- app.js
+++ app.js
@@ -207,7 +207,7 @@ async function initializeApp() {
     });
 
     app.get("/", (req, res) => {
-      res.json({ message: "Hello from our StorageApp" });
+      res.json({ message: "Hello from our2 StorageApp" });
     });
 
     // Routes
